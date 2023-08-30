78f48b7f1cf57b3db2c93d0e6aa47e135eb15380
mdelvecchio <mdelvecchio3119@gmail.com>
2023-08-29 22:31:16 -0400

    landscaper: user can buy and use rusty scissors to cut grass

--- landscaper.py
+++ landscaper.py
@@ -14,13 +14,28 @@ while(True):
     user_input = int(input(""" 
                         What would you like to do?
                         [1] Cut Grass
-                        [2] Quit
+                        [2] Upgrade Tool
+                        [3] Quit
                         """))
     if (user_input == 1):
-        landscaper_data['money'] += 1
-        print(f"You made $1 cutting grass with your teeth! You now have ${landscaper_data['money']}.")
+        if (landscaper_data['tools'] == "teeth"):
+            landscaper_data['money'] += 1
+            print(f"You made $1 cutting grass with your teeth! You now have ${landscaper_data['money']}.")
+        
+        if (landscaper_data['tools'] == "rusty scissors"):
+            landscaper_data['money'] += 5
+            print(f"You made $5 cutting grass using the rusty scissors! You now have ${landscaper_data['money']}.")
 
     if (user_input == 2):
+        if (landscaper_data['tools'] == "teeth" and landscaper_data['money'] >= 5):
+            print("You upgraded to rusty scissors! You now make $5 cutting grass!")
+
+            landscaper_data['money'] -= 5
+            landscaper_data['tools'] = "rusty scissors"
+        else:
+            print("You can't upgrade at this time. Come back later.")
+
+    if (user_input == 3):
         landscaper_data['quit'] = True
 
     if (landscaper_data['quit'] is True):
